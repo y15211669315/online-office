513c1285bfb1537c8d98646bc8decfba9a5efef9
admin <15211669315@sina.cn>
2019-05-24 18:37:12 +0800

    添加注释测试

--- src/main/java/com/onlineoffice/OnlineOfficeApplication.java
+++ src/main/java/com/onlineoffice/OnlineOfficeApplication.java
@@ -3,11 +3,23 @@ package com.onlineoffice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Package: com.onlineoffice
+ * @FileName: OnlineOfficeApplication.java
+ * @ClassName: OnlineOfficeApplication
+ * @Description: SpringBoot启动引擎
+ * @Author: yeshengguang
+ * @CreateDate: 2019/5/24 18:36
+ * @UpdateUser: yeshengguang
+ * @UpdateDate: 2019/5/24 18:36
+ * @UpdateRemark: 说明本次修改内容
+ * @Version: v1.0
+ */
 @SpringBootApplication
 public class OnlineOfficeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineOfficeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineOfficeApplication.class, args);
+    }
 
 }
